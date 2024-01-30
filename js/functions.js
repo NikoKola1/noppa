e31c3e8227eb7e1063feae5ed9706be42766127a
NikoKola1 <n3koni00@students.oamk.fi>
2024-01-30 16:48:50 +0200

    valmis noppa tehtävä

---
+++ js/functions.js
@@ -0,0 +1,12 @@
+
+const getRandomIntNumberInRange = (min,max) => {
+    return Math.floor(Math.random() * max) + min;
+}
+
+document.getElementById('dice').addEventListener('click',() => {
+    const randomized_number = getRandomIntNumberInRange(1, 6);
+    
+    document.getElementById('kuva').setAttribute("src",
+     "/kuvat/inverted-dice-" + randomized_number + ".png");
+});
+
